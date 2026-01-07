766c9dc65a9483a951ce6d5af4df0edc8d9513dd
JorgeRM7 <jorgerinconm76@gmail.com>
2026-01-07 14:15:19 -0600

    Initial commit

--- app/Jobs/SendInvoiceToNetSuiteJob.php
+++ app/Jobs/SendInvoiceToNetSuiteJob.php
@@ -2,26 +2,49 @@
 
 namespace App\Jobs;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class SendInvoiceToNetSuiteJob implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public int $invoice;
 
     /**
-     * Create a new job instance.
+     * Número de intentos
      */
-    public function __construct()
+    public int $tries = 3;
+
+    /**
+     * Timeout del job (segundos)
+     */
+    public int $timeout = 120;
+
+    /**
+     * Crear nueva instancia del Job
+     */
+    public function __construct(int $invoice)
     {
-        //
+        $this->invoice = $invoice;
     }
 
     /**
-     * Execute the job.
+     * Ejecutar el Job
      */
     public function handle(): void
     {
-        //
+        Log::info("Job SendInvoiceToNetSuiteJob iniciado", [
+            'invoice' => $this->invoice
+        ]);
+
+
+        Log::info("Job SendInvoiceToNetSuiteJob finalizado", [
+            'invoice' => $this->invoice
+        ]);
     }
 }
